61f193b3b03b79e8dc43fa0e2f51e21d7f05a898
jillianblake <71796537+jillianblake@users.noreply.github.com>
2021-03-26 13:12:26 -0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,11 +13,13 @@ const Banner = (props) => (
 
             <header className="major">
                 <h1>Hello, welcome to Blake Immigration Law.</h1>
+  <i><h1>Hola, bienvenidos a Blake Immigration Law.</h1></i>
             </header>
             <div className="content">
-                <p>WE SPECIALIZE IN REMOVAL DEFENSE, ASYLUM, AND FAMILY-BASED IMMIGRATION.</p>
+                <p>WE SPECIALIZE IN REMOVAL DEFENSE, ASYLUM, AND FAMILY-BASED IMMIGRATION.
+ <i><br>NOS ESPECIALIZAMOS EN DEFENSA CONTRA DEPORTACIÓN, ASILO E INMIGRACIÓN FAMILIAR.</p></i>
                 <ul className="actions">
-                    <li><a href="tel:7035947857" className="button next scrolly">Call Us (703) 594-7857</a></li>
+                    <li><a href="tel:7035947857" className="button next scrolly">Call Us <br> <i>Llámanos</i> (703) 594-7857</a></li>
                 </ul>
             </div>
         </div>
