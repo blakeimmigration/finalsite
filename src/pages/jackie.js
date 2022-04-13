c1c00160775099ea209921cd1cae8774b12b4396
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-04-12 22:17:34 -0400

    Update jackie.js

--- src/pages/jackie.js
+++ src/pages/jackie.js
@@ -1,1 +1,32 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
+import pic11 from '../assets/images/pic40.jpg'
+
+const Jackie= (props) => (
+    <Layout>
+        <Helmet>
+            <title>Jackie</title>
+            <meta name="description" content="Jackie Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Jackie Citalan</h1>
+    <h3>Senior Paralegal</h3>
+                    </header>
+                    <img src={pic40} alt="" />
+                    <p>Bio coming soon...</p>
+    <i><p>...</p></i> 
+            
+    </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Jackie
