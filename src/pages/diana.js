b7ec47cbc0c6914b4e5ac794df0734e94efe2461
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-04-12 22:02:07 -0400

    Update diana.js

--- src/pages/diana.js
+++ src/pages/diana.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic22 from '../assets/images/pic22.jpg'
+import pic39 from '../assets/images/pic39.jpg'
 
 const Diana= (props) => (
     <Layout>
@@ -18,7 +18,7 @@ const Diana= (props) => (
                         <h1>Diana Maravilla</h1>
     <h3>Receptionist/Administrative Assistant</h3>
                     </header>
-                    <img src={pic22} alt="" />
+                    <img src={pic39} alt="" />
                     <p> Diana has been a receptionist and administrative assistant at Blake Immigration Law since 2021. Diana grew up in Arlington, Virginia and earned her Immigration Law Specialist Paralegal Certificate at the Washington Online Learning Institute. She previously worked as a receptionist and assistant at medical and immigration law offices in northern Virginia. In her free time Diana likes to read, garden, and bake. She also likes to spend time with her family and friends, and her dog, Kiko. </p>
     <i><p>Diana ha sido recepcionista y asistente administrativa en Blake Immigration Law desde 2021. Diana creció en Arlington, Virginia y obtuvo su Certificado de asistente legal de especialista en derecho de inmigración del Washington Online Learning Institute. Anteriormente trabajó como recepcionista y asistente en oficinas médicas y de inmigración en el norte de Virginia. En su tiempo libre, a Diana le gusta leer, trabajar en el jardín, y hornear. También le gusta pasar tiempo con su familia y amigos, y con su perro, Kiko.</p></i>
                 </div>
