add42bca78db84f4840f56eb5d272b95bf15c050
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-04-13 23:48:14 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -143,24 +143,6 @@ const About = (props) => (
                     </div>
                 </section>
 
-<section>
-                    <Link to="/jackie" className="image">
-                        <img src={pic40} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Jackie Citalan</h3>
-                                <h4>Receptionist/Administrative Assistant</h4>
-                            </header>
-                            <p>More information coming soon...</a> </p>
-    <i><p>Más información próximamente... </a></p></i>
-                            <ul className="actions">
-                                <li><a href="mailto:jackie@blakeimmigration.com" className="button fit">jackie@blakeimmigration.com</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
            </section>
         </div>
 
