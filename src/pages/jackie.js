9e2fcad8ecdbf8f614c481b9eef4d797e342cce7
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2022-04-19 22:02:33 -0400

    Update jackie.js

--- src/pages/jackie.js
+++ src/pages/jackie.js
@@ -19,8 +19,8 @@ const Jackie = (props) => (
     <h3>Receptionist/Administrative Assistant</h3>
                     </header>
                     <img src={pic40} alt="" />
-                    <p>Jackie has been a receiptionist and administrative assistant at Blake Immigration Law since 2022.</p>
-    <i><p>Jackie ha sido recepcionista y asistente administrativa en Blake Immigration Law desde 2022.</p></i>
+                    <p>Jackie has been a receiptionist and administrative assistant at Blake Immigration Law since 2022. Before joining Blake Immigration Law Jackie was a bilingual patient coordinator at a dental office in Virginia. Jackie enjoys working in immigration law because she is able to help people from all different backgrounds and understands the importance of giving clients the best attention and service. In her free time Jackie likes to listen to music, watch tv and movies, and spend time with her family and friends.</p>
+    <i><p>Jackie ha sido recepcionista y asistente administrativa en Blake Immigration Law desde 2022. Antes de unirse a Blake Immigration Law, Jackie fue coordinadora bilingüe de pacientes en un consultorio dental en Virginia. Jackie disfruta trabajar en derecho de inmigración porque puede ayudar a personas de diferentes orígenes y comprende la importancia de brindar a los clientes la mejor atención y servicio. En su tiempo libre, a Jackie le gusta escuchar música, ver televisión y películas, y pasar tiempo con su familia y amigos.</p></i>
                     
                 </div>
             </section>
