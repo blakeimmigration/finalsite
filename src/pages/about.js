4414b51a9bb463fb95843b5b43156547aae3bc3b
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2023-01-02 18:41:57 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -143,24 +143,7 @@ const About = (props) => (
                         </div>
                     </div>
                 </section>
-<section>
-                    <Link to="/sarai" className="image">
-                        <img src={pic41} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Sarai Valles-Alberto</h3>
-                                <h4>Paralegal</h4>
-                            </header>
-                            <p>Bio coming soon.</p>
-    
-                            <ul className="actions">
-                                <li><a href="mailto:sarai@blakeimmigration.com" className="button fit">sarai@blakeimmigration.com</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+
 <section>
                     <Link to="/jackie" className="image">
                         <img src={pic40} alt="" />
