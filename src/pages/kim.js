53ab5f4a838ebfc6b5f6fd7b223510a30b95c6e9
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2023-06-27 19:42:46 -0400

    Create kim.js

---
+++ src/pages/kim.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+import pic43 from '../assets/images/pic43.jpg'
+
+const kim = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Kim Whatley</title>
+            <meta name="description" content="Kim Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Kim Whatley</h1>
+    <h3>Receptionist/Administrative Assistant</h3>
+                    </header>
+                    <img src={pic43} alt="" />
+                    <p>Bio coming soon.</p>
+
+                    
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default kim
