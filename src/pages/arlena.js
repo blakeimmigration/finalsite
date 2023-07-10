f91598ad4856e31b501b359792f51b2bb08ef798
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2023-07-09 21:01:44 -0400

    Create arlena.js

---
+++ src/pages/arlena.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+import pic45 from '../assets/images/pic45.jpg'
+
+const Arlena= (props) => (
+    <Layout>
+        <Helmet>
+            <title>Arlena Lara Quinteros</title>
+            <meta name="description" content="Wendy Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Arlena Lara Quinteros</h1>
+    <h3>Paralegal</h3>
+                    </header>
+                    <img src={pic44} alt="" />
+                    <p> Bio coming soon. </p>
+   
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Arlena
