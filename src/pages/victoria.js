6e7b6fb557c7d24303102409b424fc150d417820
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2025-01-09 19:47:33 -0500

    Update victoria.js

--- src/pages/victoria.js
+++ src/pages/victoria.js
@@ -1,1 +1,32 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
+import pic54 from '../assets/images/pic54.jpg'
+
+const Shane = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Shane</title>
+            <meta name="description" content="Victoria Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Victoria Grimm</h1>
+    <h3>Associate Attorney</h3>
+                    </header>
+                    <img src={pic54} alt="" />
+                    <p>Victoria is a law clerk at Blake Immigration Law.</p>
+<i><p>Victoria is a law clerk at Blake Immigration Law.</p></i>
+                   
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Victoria
