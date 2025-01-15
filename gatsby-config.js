d5b401b57cd527b46848ef164f0a962e935a2815
Rémi Rucojevic <remi.rucojevic@gmail.com>
2025-01-16 02:25:55 +0700

    update form node-sass to sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,12 @@ module.exports = {
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
+    },
     'gatsby-plugin-offline'
   ],
 }
