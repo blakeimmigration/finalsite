aebc7ab048e4a0c0dd37eb8b752f3023f0d44c03
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2025-01-20 17:57:10 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,6 +18,7 @@ import pic44 from '../assets/images/pic44.jpg'
 import pic45 from '../assets/images/pic45.jpg'
 import pic54 from '../assets/images/pic54.jpg'
 import pic55 from '../assets/images/pic55.jpg'
+import pic71 from '../assets/images/pic71.jpg'
 
 const About = (props) => (
     <Layout>
@@ -256,6 +257,24 @@ const About = (props) => (
     <i><p>Biografía próximamente...<a href="https://blakeimmigration.netlify.app/sara/">Leer más sobre Sara...</a></p></i>
                             <ul className="actions">
                                 <li><a href="mailto:sara@blakeimmigration.com" className="button fit">sara@blakeimmigration.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+     <section>
+                    <Link to="/sashenka" className="image">
+                        <img src={pic55} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Sashenka Fernandez</h3>
+<h4>Administrative Assistant</h4>
+                            </header>
+                            <p>Bio coming soon...<a href="https://blakeimmigration.netlify.app/sashenka/">Read more about Sashenka...</a></p>
+    <i><p>Biografía próximamente...<a href="https://blakeimmigration.netlify.app/sara/">Leer más sobre Sashenka...</a></p></i>
+                            <ul className="actions">
+                                <li><a href="mailto:sashenka@blakeimmigration.com" className="button fit">sashenka@blakeimmigration.com</a></li>
                             </ul>
                         </div>
                     </div>
