bd3b563fba9a92c4371d9340246ae9d76258204a
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2025-04-17 10:27:08 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -119,8 +119,8 @@ const About = (props) => (
                                 <h3>Victoria Grimm</h3>
 <h4>Law Clerk</h4>
                             </header>
-                            <p>Bio Coming Soon...<a href="https://blakeimmigration.netlify.app/victoria/">Read more about Victoria...</a></p>
-    <i><p>Biografía pronto...<a href="https://blakeimmigration.netlify.app/victoria/">Leer más sobre Victoria... </a></p></i>
+                            <p>Victoria has been a law clerk at Blake Immigration Law since 2024. Victoria grew up nearby in Fairfax, Virginia. She earned her B.A. in international relations from Virginia Tech and law degree from George Washington University Law School. <a href="https://blakeimmigration.netlify.app/victoria/">Read more about Victoria...</a></p>
+    <i><p>Victoria ha sido asistente legal en Blake Immigration Law desde 2024. Victoria creció en Fairfax, Virginia. Obtuvo su licenciatura en relaciones internacionales de Virginia Tech y su doctorada en derecho de la Facultad de Derecho de la Universidad George Washington.<a href="https://blakeimmigration.netlify.app/victoria/">Leer más sobre Victoria... </a></p></i>
                             <ul className="actions">
                                 <li><a href="mailto:victoria@blakeimmigration.com" className="button fit">victoria@blakeimmigration.com</a></li>
                             </ul>
