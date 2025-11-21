415c92a87a4490cc2bfe88ed4751da2e70c75d9b
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2025-11-20 19:00:18 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -225,24 +225,7 @@ const About = (props) => (
                         </div>
                     </div>
                 </section>
-    <section>
-                    <Link to="/sara" className="image">
-                        <img src={pic55} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Sara Sanchez</h3>
-<h4>Administrative Assistant</h4>
-                            </header>
-                            <p>Sara has been an administrative assistant at Blake Immigration Law since 2024. Sara grew up in Colombia and immigrated to the United States in 2021. She studied law at the Universidad Autónoma Latinoamericana in Medellín, Colombia.<a href="https://blakeimmigration.netlify.app/sara/">Read more about Sara...</a></p>
-    <i><p>Sara ha sido asistente administrativa en Blake Immigration Law desde 2024. Sara creció en Colombia y imigró a los Estados Unidos en 2021. Estudió derecho en la Universidad Autónoma Latinoamericana en Medellín, Colombia.<a href="https://blakeimmigration.netlify.app/sara/">Leer más sobre Sara...</a></p></i>
-                            <ul className="actions">
-                                <li><a href="mailto:sara@blakeimmigration.com" className="button fit">sara@blakeimmigration.com</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+    
    
        </section>
 
