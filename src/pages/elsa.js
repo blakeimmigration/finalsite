2f457805ae90725b9f4f309800f0bc485d4a1964
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2025-11-24 13:55:29 -0500

    Update elsa.js

--- src/pages/elsa.js
+++ src/pages/elsa.js
@@ -1,1 +1,31 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
 
+import pic73 from '../assets/images/pic73.jpg'
+
+const Elsa= (props) => (
+    <Layout>
+        <Helmet>
+            <title>Elsa Corea Largaespada</title>
+            <meta name="description" content="Elsa Page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Elsa Corea Largaespada</h1>
+    <h3>Administrative Assistant</h3>
+                    </header>
+                    <img src={pic73} alt="" />
+                    <p> Bio coming soon... </p>
+  
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Elsa
