42ab9cb7001876e1f3e1c647169db0a88bcd8f13
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2026-09-23 18:37:01 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -110,24 +110,7 @@ const About = (props) => (
                         </div>
                     </div>
                 </section>
- <section>
-                    <Link to="/victoria" className="image">
-                        <img src={pic54} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Victoria Grimm</h3>
-<h4>Associate Attorney</h4>
-                            </header>
-                            <p>Victoria has worked at Blake Immigration Law since 2024, first as a law clerk and now as an attorney. Victoria grew up nearby in Fairfax, Virginia. She earned her B.A. in international relations from Virginia Tech and law degree from George Washington University Law School.<a href="https://blakeimmigration.netlify.app/victoria/">Read more about Victoria...</a></p>
-    <i><p>Victoria ha trabajado en Blake Immigration Law desde 2024 primeramente como asistente legal y ahora como abogada. Victoria creció en Fairfax, Virginia. Obtuvo su licenciatura en relaciones internacionales de Virginia Tech y su doctorada en derecho de la Facultad de Derecho de la Universidad George Washington.<a href="https://blakeimmigration.netlify.app/victoria/">Leer más sobre Victoria... </a></p></i>
-                            <ul className="actions">
-                                <li><a href="mailto:victoria@blakeimmigration.com" className="button fit">victoria@blakeimmigration.com</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+ 
                     <section>
                     <Link to="/karina" className="image">
                         <img src={pic11} alt="" />
